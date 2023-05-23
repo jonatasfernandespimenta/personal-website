1e4cb1c11d916b8802b1f72d416c1b80c4025bee
jonatasfernandespimenta <jonatas.pimenta@copernico.com.br>
2023-05-23 14:09:04 -0300

    :sparkles: adds aboutme section

--- src/sections/AboutMeSection.tsx
+++ src/sections/AboutMeSection.tsx
@@ -2,6 +2,7 @@ import Heading from "@/components/Heading";
 import SubHeading from "@/components/SubHeading";
 import { techs } from "@/utils/techs";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function AboutMeSection() {
   return (
@@ -39,14 +40,24 @@ export default function AboutMeSection() {
         <div className="flex flex-1 justify-around h-screen items-center p-36 flex-col">
           <Image src="/assets/pfp.png" alt="pfp" width={400} height={400} />
 
-          <div className="flex flex-col items-center">
+          <div className="flex flex-col items-center gap-5">
             <SubHeading textColor="green-400" textSize="3xl">
               Techs I use:
             </SubHeading>
 
             <div className="flex flex-row gap-4">
               {techs.map((tech) => (
-                <Image src={`/assets/techs/${tech.name}.png`} alt={tech.name} width={60} height={60} />
+                <div className="cursor-pointer flex items-center">
+                  <Link href={tech.link} target="_blank">
+                    <Image
+                      src={`/assets/techs/${tech.name}.png`}
+                      alt={tech.name}
+                      width={60}
+                      height={60}
+                      className="transition-transform duration-100 transform-gpu hover:scale-110"
+                    />
+                  </Link>
+                </div>
               ))}
             </div>
           </div>
--- src/utils/techs.ts
+++ src/utils/techs.ts
@@ -1,17 +1,22 @@
 export const techs = [
   {
     name: "react",
+    link: "https://react.dev/",
   },
   {
     name: "node",
+    link: "https://nodejs.org/en",
   },
   {
     name: "nest",
+    link: "https://nestjs.com/",
   },
   {
     name: "python",
+    link: "https://www.python.org/",
   },
   {
     name: "typescript",
+    link: "https://www.typescriptlang.org/",
   },
 ];
